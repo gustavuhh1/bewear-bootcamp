d0b0de21fbae4e65f582ab0bcfb4abc142a06f22
GustavoMR <gustavomrodrigues11@gmail.com>
2025-09-03 11:49:27 -0300

    feat: add link for homepage in Header

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -19,7 +19,9 @@ export const Header = () => {
 
   return (
     <header className="flex items-center justify-between p-5">
+      <Link href="/" >
       <Image src={"/logo.svg"} alt="BEWEAR" width={100} height={26.14} />
+      </Link>
       <div className="flex items-center">
         <Button variant={"outline"} size={"icon"}>
           <Sheet>
